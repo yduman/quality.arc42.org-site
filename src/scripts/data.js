181e8c5e06f9a4adb0c4b8c9dab75a8319027576
Yadullah Duman <yadullah.duman@gmail.com>
2025-02-09 22:46:45 +0100

    add script to generate graph data from markdown sources

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -3,9 +3,35 @@ import { promises as fs } from "node:fs";
 import path from "node:path";
 import process from "node:process";
 
+/**
+ * @typedef {Object} FrontmatterData
+ * @property {string} title - The page title
+ * @property {string} tags - Space-separated string, listing system properties
+ * @property {string} related - Comma-separated string, listing qualities
+ * @property {string} permalink - The link to the page
+ * @property {string} [stakeholder] - Comma-separated string, listing stakeholders needing this requirement
+ */
+
+/**
+ * @typedef {Object} Q42Node
+ * @property {string} id - Node ID
+ * @property {string} label - Node display name
+ * @property {number} size - Node size
+ * @property {string} color - Node color
+ * @property {string} qualityType - Node quality type
+ * @property {string} page - Link to documentation
+ */
+
+/**
+ * @typedef {Object} Q42Edge
+ * @property {string} source - Edge source
+ * @property {string} target - Edge target
+ */
+
 const projectRoot = process.cwd();
 const qualitiesDir = path.join(projectRoot, "qualities");
 const requirementsDir = path.join(projectRoot, "requirements");
+const assetsDir = path.join(projectRoot, "assets");
 
 let [qualityFiles, requirementFiles] = await Promise.all([
   getFilePaths(qualitiesDir),
@@ -20,8 +46,86 @@ const [qualityData, requirementsData] = await Promise.all([
   parseFrontmatter(requirementFiles),
 ]);
 
-console.log(qualityData.length);
-console.log(requirementsData.length);
+/** @type {Map<string, Q42Node>} */
+const propertyNodes = new Map();
+
+/** @type {Set<Q42Node>} */
+const nodes = new Set();
+
+/** @type {Set<Q42Edge>} */
+const edges = new Set();
+
+createGraphData(qualityData);
+createGraphData(requirementsData, true);
+
+const propertyNodesJson = JSON.stringify(Array.from(propertyNodes.values()), null, 2);
+const nodesJson = JSON.stringify(Array.from(nodes), null, 2);
+const edgesJson = JSON.stringify(Array.from(edges), null, 2);
+
+await Promise.all([
+  writeJsonToFile(propertyNodesJson, "property-nodes.json"),
+  writeJsonToFile(nodesJson, "nodes.json"),
+  writeJsonToFile(edgesJson, "edges.json"),
+]);
+
+/**
+ * Write JSON to assets
+ * @param {string} jsonString
+ * @param {string} filename
+ */
+async function writeJsonToFile(jsonString, filename) {
+  const outputPath = path.join(assetsDir, "data", filename);
+
+  try {
+    await fs.writeFile(outputPath, jsonString, "utf8");
+    console.log(`File written successfully to ${outputPath}`);
+  } catch (error) {
+    console.error(`Error writing file: ${error}`);
+  }
+}
+
+/**
+ * Create and write a JSON file that represents all types of quality nodes
+ * @param {FrontmatterData[]} frontmatterData
+ */
+function createGraphData(frontmatterData, isRequirements = false) {
+  for (const data of frontmatterData) {
+    const id = data.permalink.split("/").pop();
+    const tags = data.tags ? data.tags.split(" ").map((t) => t.trim()) : [];
+    const relatedIds = data.related ? data.related.split(",").map((r) => r.trim()) : [];
+
+    for (const tag of tags) {
+      // TODO: check if we want to connect requirements to system properties
+      if (!isRequirements) {
+        edges.add({ source: id, target: tag });
+      }
+
+      if (!propertyNodes.has(tag)) {
+        propertyNodes.set(tag, {
+          id: tag,
+          label: tag.charAt(0).toUpperCase() + tag.slice(1),
+          size: 15,
+          color: "green",
+          qualityType: "property",
+          page: `/tag-${tag}`,
+        });
+      }
+    }
+
+    for (const relatedId of relatedIds) {
+      edges.add({ source: id, target: relatedId });
+    }
+
+    nodes.add({
+      id,
+      label: data.title,
+      size: 15,
+      color: isRequirements ? "yellow" : "blue",
+      qualityType: isRequirements ? "requirement" : "quality",
+      page: data.permalink,
+    });
+  }
+}
 
 /**
  * Parses the frontmatter data for a given Markdown file
